1ba765cc5edbfd374a5f17a88d6f21956de1edad
amithay12 <amithayot12@gmail.com>
2024-12-18 23:54:45 +0200

    updating_files

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+var app = express()
+
+const articles = require('./routes/articles');
+app.use('/articles', articles);
+
+app.set('view engine, ejs')
+
+app.listen(8080)
+
---
+++ controllers/articles.js
@@ -0,0 +1,12 @@
+
+    
+    const articles = require('../models/articles')
+
+    const getArticle = (req,res) => {
+        const id = req.params.id
+        res.render('article.ejs', { foo: articles.getArticle(id) })
+    }
+  
+  module.exports = {
+    getArticle
+  };
